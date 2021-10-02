9221e8a3138aec21b016ab8943849e5b11ad11cd
Elizabeth Connacher <70343514+econnacher@users.noreply.github.com>
2021-10-02 10:59:24 -0700

    Update DataMemory_tb.v

--- DataMemory_tb.v
+++ DataMemory_tb.v
@@ -30,38 +30,35 @@ module DataMemory_tb();
 		Clk <= 1'b0;
 		forever #10 Clk <= ~Clk;
 	end
-
-	initial begin
-	
-    /* Please fill in the implementation here... */
-    Address <= 32'b0;
-    WriteData <= 32'b0; 
-    MemWrite <= 0;
-    MemRead <= 0;
-	
-	#2
-	//write to memory
-	WriteData <= 1; 
-	MemWrite <= 1;
-	#20
-	WriteData <= 0;
-	MemWrite <= 0;
-	MemRead <= 1;
-	#40
-	MemRead <= 0;
-	
-	//write to memory
-	#10 
-	Address <= 4;
-	WriteData <= 2;
-	MemWrite <= 1;
-	#20
-	//read from memory
-	MemWrite <= 0;
-    MemRead <= 1;
-    #20
-    MemRead <= 0;
     
+    integer i;
+	initial begin
+	//initialize to zero
+	Address <= 32'b0;
+    	WriteData <= 32'b0; 
+   	MemWrite <= 0;
+    	MemRead <= 0;
+	#10
+	//initialize memory 0 - 10
+	MemWrite <= 1;
+	for(i = 0; i < 10; i = i+1) begin
+	   Address <= i*4;
+	   WriteData <= i*10;
+	   @(posedge Clk);
+	   #10;
 	end
+    	MemWrite <= 0;
+    	WriteData <= 0;
+    	#10;
+    
+	//read from memory 0 - 10
+	for(i = 0; i < 10; i = i+1) begin
+	   Address <= i*4;
+	   MemRead <= 1;
+	   #10;
+	   MemRead <= 0;
+	   #10;
+	end
+    end
 
 endmodule
