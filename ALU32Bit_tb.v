156fae749b9f9f31d9bb001ddb69217ec4a12919
Elizabeth Connacher <70343514+econnacher@users.noreply.github.com>
2021-10-04 14:06:17 -0700

    Update ALU32Bit_tb.v

--- ALU32Bit_tb.v
+++ ALU32Bit_tb.v
@@ -26,8 +26,66 @@ module ALU32Bit_tb();
 	initial begin
 	
     /* Please fill in the implementation here... */
+	//Setting up two numbers
+	A <= 716;
+	B <= 26;
 	
+	//Test0: AND
+	ALUControl <= 0;
+	#20;
+	
+	//Test1: OR
+	ALUControl <= 1;
+	#20;
+	
+	//Test2: ADD
+	ALUControl <= 2;
+	#20;
+	
+	//Test3: XOR
+	ALUControl <= 3;
+	#20;
+	
+	//Test4: SLL
+	ALUControl <= 4;
+	#20;
+	
+	//Test5: SRL
+	ALUControl <= 5;
+	#20;
+	
+	//Test6: SUBTRACT
+	ALUControl <= 6;
+	#20;
+	
+	//Test7: SLT
+	ALUControl <= 7;
+	#20;
+	A <= 15;
+	#20
+	
+	//Test8: ROTATE LEFT
+	ALUControl <= 8;
+	#20;
+	A <= 32'b11011101011010101101010110011010;
+	#20;
+	
+	//Test9: ROTATE RIGHT
+	ALUControl <= 9;
+	#20;
+	
+	//Test12: NOR
+	A <= 19;
+	ALUControl <= 12;
+	#20;
+	
+	//Test14: ERROR
+	ALUControl <= 14;
+	#20;
+	
+	//Test15: MULTIPLY
+	ALUControl <= 15;
+    
 	end
 
 endmodule
-
