10e5d4bf6362367161df14fb7349dd1ea1b1605c
Elizabeth Connacher <70343514+econnacher@users.noreply.github.com>
2021-10-23 15:04:32 -0700

    Update DataMemory_tb.v

--- DataMemory_tb.v
+++ DataMemory_tb.v
@@ -35,30 +35,53 @@ module DataMemory_tb();
 	initial begin
 	//initialize to zero
 	Address <= 32'b0;
-    	WriteData <= 32'b0; 
-   	MemWrite <= 0;
-    	MemRead <= 0;
+    WriteData <= 32'b0; 
+    MemWrite <= 0;
+    MemRead <= 0;
 	#10
 	//initialize memory 0 - 10
-	MemWrite <= 1;
-	for(i = 0; i < 10; i = i+1) begin
-	   Address <= i*4;
-	   WriteData <= i*10;
-	   @(posedge Clk);
-	   #10;
-	end
-    	MemWrite <= 0;
-    	WriteData <= 0;
-    	#10;
+	//MemWrite <= 1;
+	//for(i = 0; i < 10; i = i+1) begin
+	//   Address <= i*4;
+	//   WriteData <= i*10;
+	//   @(posedge Clk);
+	//   #10;
+	//end
+    //MemWrite <= 0;
+    //WriteData <= 0;
+    //#10;
     
 	//read from memory 0 - 10
-	for(i = 0; i < 10; i = i+1) begin
+	for(i = 0; i < 10; i=i+1) begin
 	   Address <= i*4;
 	   MemRead <= 1;
 	   #10;
 	   MemRead <= 0;
 	   #10;
 	end
-    end
+	/*
+	WriteData <= 1; 
+	MemWrite <= 1;
+	#20
+	WriteData <= 0;
+	MemWrite <= 0;
+	MemRead <= 1;
+	#40
+	MemRead <= 0;
+	
+	//write to memory
+	#10 
+	Address <= 4;
+	WriteData <= 2;
+	MemWrite <= 1;
+	#20
+	//read from memory
+	MemWrite <= 0;
+    MemRead <= 1;
+    #20
+    MemRead <= 0;
+    */
+    
+	end
 
 endmodule
