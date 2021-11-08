151f479e0111079a56322983f168e0ce9ef7cd02
Elizabeth Connacher <70343514+econnacher@users.noreply.github.com>
2021-11-08 16:10:52 -0700

    Update HazardDetection.v

--- HazardDetection.v
+++ HazardDetection.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
 module HazardDetection(    
-    ID_EX_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, MEM_WB_RegWrite,
-    IF_ID_Rs, IF_ID_Rt, ID_EX_Rt, ID_EX_Rd, IF_ID_Rs, IF_ID_Rt, EX_MEM_Rd, MEM_WB_Rd,
-    stall_ID_EX, stall_EX_MEM, stall_MEM_WB,
-    IF_ID_Write, PCWrite, IF_Flush 
+    IDEX_MemRead, IDEX_RegWrite, EXMEM_RegWrite, MEMWB_RegWrite,
+    IFID_Rs, IFID_Rt, IDEX_Rt, IDEX_Rd, IFID_Rs, IFID_Rt, EXMEM_Rd, MEMWB_Rd,
+    stall_IDEX, stall_EXMEM, stall_MEMWB,
+    IFID_Write, PCWrite, IF_Flush 
     );
     
-    input ID_EX_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, MEM_WB_RegWrite;
-    input [4:0] IF_ID_Rs, IF_ID_Rt, ID_EX_Rt, ID_EX_Rd, IF_ID_Rs, IF_ID_Rt, EX_MEM_Rd, MEM_WB_Rd;
-    output reg stall_ID_EX, stall_EX_MEM, stall_MEM_WB; // 1 to stall/insert zeros
-    output reg IF_ID_Write, PCWrite, IF_Flush; // 1 means to do the action implied 
+    input IDEX_MemRead, IDEX_RegWrite, EXMEM_RegWrite, MEMWB_RegWrite;
+    input [4:0] IFID_Rs, IFID_Rt, IDEX_Rt, IDEX_Rd, IFID_Rs, IFID_Rt, EXMEM_Rd, MEMWB_Rd;
+    output reg stall_IDEX, stall_EXMEM, stall_MEMWB; // 1 to stall/insert zeros
+    output reg IFID_Write, PCWrite, IF_Flush; // 1 means to do the action implied 
     
     initial begin
     //nothin yet
@@ -19,27 +19,27 @@ module HazardDetection(
     always @(*) begin
     //fix what would normally be caught by forwarding
     //r-type followed by another r-type
-    if((ID_EX_RegWrite == 1) && ((ID_EX_Rd == IF_ID_Rs) | (ID_EX_Rd == IF_ID_Rt))) begin
-        stall_ID_EX = 1;
-        stall_EX_MEM = 0;
-        stall_MEM_WB = 0;
-        IF_ID_Write = 0;
+    if((IDEX_RegWrite == 1) && ((IDEX_Rd == IFID_Rs) | (IDEX_Rd == IFID_Rt))) begin
+        stall_IDEX = 1;
+        stall_EXMEM = 0;
+        stall_MEMWB = 0;
+        IFID_Write = 0;
         PCWrite = 0;
         IF_Flush = 0;
     end
-    if((EX_MEM_RegWrite == 1) && ((EX_MEM_Rd == IF_ID_Rs) | (EX_MEM_Rd == IF_ID_Rt))) begin
-        stall_ID_EX = 0;
-        stall_EX_MEM = 1;
-        stall_MEM_WB = 0;
-        IF_ID_Write = 0;
+    if((EXMEM_RegWrite == 1) && ((EXMEM_Rd == IFID_Rs) | (EXMEM_Rd == IFID_Rt))) begin
+        stall_IDEX = 0;
+        stall_EXMEM = 1;
+        stall_MEMWB = 0;
+        IFID_Write = 0;
         PCWrite = 0;
         IF_Flush = 0;
     end
-    if((MEM_WB_RegWrite == 1) && ((MEM_WB_Rd == IF_ID_Rs) | (MEM_WB_Rd == IF_ID_Rt))) begin
-        stall_ID_EX = 0;
-        stall_EX_MEM = 0;
-        stall_MEM_WB = 1;
-        IF_ID_Write = 0;
+    if((MEMWB_RegWrite == 1) && ((MEMWB_Rd == IFID_Rs) | (MEMWB_Rd == IFID_Rt))) begin
+        stall_IDEX = 0;
+        stall_EXMEM = 0;
+        stall_MEMWB = 1;
+        IFID_Write = 0;
         PCWrite = 0;
         IF_Flush = 0;
     end
@@ -47,11 +47,11 @@ module HazardDetection(
     
     //normal hazard detection
     //lw
-    if(ID_EX_MemRead==1 && ((IF_ID_Rs==ID_EX_Rt) | (IF_ID_Rt==ID_EX_Rt))) begin
-        stall_ID_EX = 1;
-        stall_EX_MEM = 0;
-        stall_MEM_WB = 0;
-        IF_ID_Write = 0;
+    if(IDEX_MemRead==1 && ((IFID_Rs==IDEX_Rt) | (IFID_Rt==IDEX_Rt))) begin
+        stall_IDEX = 1;
+        stall_EXMEM = 0;
+        stall_MEMWB = 0;
+        IFID_Write = 0;
         PCWrite = 0;
         IF_Flush = 0;
     end
